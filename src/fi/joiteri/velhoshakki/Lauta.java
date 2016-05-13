9c6fecc48a4d0c5fb209dd78dafc1ee885017906
Olli-Petteri Hirvonen <olli.hirvonen@pp4.inet.fi>
2016-05-13 10:59:35 +0300

    Paketin refaktorointi

--- src/fi/joiteri/velhoshakki/Lauta.java
+++ src/fi/joiteri/velhoshakki/Lauta.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package velhoshakki;
+package fi.joiteri.velhoshakki;
 
 /**
  *
  * @author Olli
  */
-public class Tarkastaja {
+public class Lauta {
     
 }
--- src/fi/joiteri/velhoshakki/VelhoShakki.java
+++ src/fi/joiteri/velhoshakki/VelhoShakki.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package velhoshakki;
+package fi.joiteri.velhoshakki;
 
 /**
  *
  * @author Olli
  */
-public class Magneetti {
+public class VelhoShakki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
